dc71945087b2496d3e21f71b2913aa07f57fc98f
20lives <lior@dotcore.co.il>
2020-11-28 12:20:24 +0200

    Importing workcation template

--- pages/index.vue
+++ pages/index.vue
@@ -1,78 +1,169 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        workcation-nuxt-i18n-rtl
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div id="app" class="min-h-screen bg-gray-200 antialiased xl:flex xl:flex-col xl:h-screen">
+    <SiteHeader class="xl:flex-shrink-0"/>
+    <div class="xl:flex-1 xl:flex xl:overflow-y-hidden">
+      <SearchFilters/>
+      <main class="py-6 xl:flex-1 xl:overflow-x-hidden">
+        <div v-for="(location, i) in locations" :class="{'mt-6': i > 0}">
+          <div class="px-4 xl:px-8">
+            <h3 class="text-gray-900 text-xl">{{ location.title }}</h3>
+            <p class="text-gray-600">{{ location.description }}</p>
+          </div>
+          <div class="mt-6 sm:overflow-x-auto sm:overflow-y-hidden">
+            <div class="px-4 sm:inline-flex sm:pt-2 sm:pb-8 xl:px-8">
+              <div v-for="(property, i) in location.properties" :class="{'mt-10 sm:ml-4': i > 0 }" class="sm:mt-0 sm:w-80 sm:flex-shrink-0">
+                <PropertyCard :property="property"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import SiteHeader from '@/components/SiteHeader'
+import SearchFilters from '@/components/SearchFilters'
+import PropertyCard from '@/components/PropertyCard'
+export default {
+  name: 'app',
+  components: {
+    SiteHeader,
+    SearchFilters,
+    PropertyCard,
+  },
+  data() {
+    return {
+      locations: [
+        {
+          title: 'Los Angeles',
+          description: "Live like the stars in these luxurious Southern California estates.",
+          properties: [
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+              beds: 3,
+              baths: 2,
+              title: "Modern home in city center",
+              price: 190000,
+              reviewCount: 34,
+              rating: 4,
+            },
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1513584684374-8bab748fbf90?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+              beds: 4,
+              baths: 1,
+              title: "Quiet living outside the city",
+              price: 175000,
+              reviewCount: 12,
+              rating: 3,
+            },
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1502005229762-cf1b2da7c5d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80',
+              beds: 5,
+              baths: 4,
+              title: "Renovated apartment uptown",
+              price: 200000,
+              reviewCount: 54,
+              rating: 5,
+            },
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+              beds: 3,
+              baths: 2,
+              title: "Family home in the suburbs",
+              price: 115000,
+              reviewCount: 34,
+              rating: 4,
+            },
+          ]
+        },
+        {
+          title: 'Phoenix',
+          description: "Escape the cold and enjoy great weather without breaking the bank.",
+          properties: [
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1447958272669-9c562446304f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80',
+              beds: 3,
+              baths: 2,
+              title: "Modern home in city center",
+              price: 190000,
+              reviewCount: 34,
+              rating: 4,
+            },
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1475855581690-80accde3ae2b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80',
+              beds: 4,
+              baths: 1,
+              title: "Quiet living outside the city",
+              price: 175000,
+              reviewCount: 12,
+              rating: 3,
+            },
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1472224371017-08207f84aaae?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+              beds: 5,
+              baths: 4,
+              title: "Renovated apartment uptown",
+              price: 200000,
+              reviewCount: 54,
+              rating: 5,
+            },
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1484154218962-a197022b5858?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+              beds: 3,
+              baths: 2,
+              title: "Family home in the suburbs",
+              price: 115000,
+              reviewCount: 34,
+              rating: 4,
+            },
+          ]
+        },
+        {
+          title: 'Dallas',
+          description: "Experience Texas living in these awesome ranch-style homes.",
+          properties: [
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1471231681582-352356ab45a0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80',
+              beds: 3,
+              baths: 2,
+              title: "Modern home in city center",
+              price: 190000,
+              reviewCount: 34,
+              rating: 4,
+            },
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1533779283484-8ad4940aa3a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80',
+              beds: 4,
+              baths: 1,
+              title: "Quiet living outside the city",
+              price: 175000,
+              reviewCount: 12,
+              rating: 3,
+            },
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1432303492674-642e9d0944b2?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+              beds: 5,
+              baths: 4,
+              title: "Renovated apartment uptown",
+              price: 200000,
+              reviewCount: 54,
+              rating: 5,
+            },
+            {
+              imageUrl: 'https://images.unsplash.com/photo-1560185007-c5ca9d2c014d?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+              beds: 3,
+              baths: 2,
+              title: "Family home in the suburbs",
+              price: 115000,
+              reviewCount: 34,
+              rating: 4,
+            },
+          ]
+        }
+      ],
+    }
+  }
+}
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,29 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '72': '18rem',
+        '80': '20rem',
+      },
+      padding: {
+        '5/6': '83.3333333%'
+      }
+    },
+    customForms: theme => ({
+      lineHeight: theme('lineHeight.snug'),
+      borderColor: 'transparent',
+      borderRadius: theme('borderRadius.lg'),
+      backgroundColor: theme('colors.gray.700'),
+      focusBorderColor: 'transparent',
+      focusShadow: 'none',
+      checkboxSize: '1.5em',
+      radioSize: '1.5em',
+      checkedColor: theme('colors.indigo.500'),
+      selectIcon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#fff"><path d="M15.3 9.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z"/></svg>`,
+    })
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/custom-forms')
+  ]
 }
